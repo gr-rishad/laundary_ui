73bbcd7e506d77e91dcc40928f90c8e60d23c6db
gr-rishad <gr.rishad@gmail.com>
2021-01-21 15:57:25 +0600

    initialize

---
+++ lib/screens/on_boarding.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class OnBoarding extends StatefulWidget {
+  @override
+  _OnBoardingState createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/bg.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
