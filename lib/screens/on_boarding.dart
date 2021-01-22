fe8acddb7b48996a8a2fa82db628129f7f33a816
gr-rishad <gr.rishad@gmail.com>
2021-01-23 01:31:10 +0600

    current page changed update

--- lib/screens/on_boarding.dart
+++ lib/screens/on_boarding.dart
@@ -6,6 +6,11 @@ class OnBoarding extends StatefulWidget {
 }
 
 class _OnBoardingState extends State<OnBoarding> {
+  int currentPage = 0;
+  final _pageController = PageController(
+    initialPage: 1,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +25,89 @@ class _OnBoardingState extends State<OnBoarding> {
               ),
             ),
           ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 500,
+                child: PageView(
+                  controller: _pageController,
+                  children: [
+                    onBoardPage("onBoard1", "Choose Clothes"),
+                    onBoardPage("onBoard2", "Schedule Pickup"),
+                    onBoardPage("onBoard3", "Get Best Iron Service"),
+                    onBoardPage("onBoard2", "Get on Time Delivery"),
+                    onBoardPage("onBoard4", "Pay Later"),
+                  ],
+                  onPageChanged: (value) => {setCurrentPage(value)},
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(5, (index) => getIndicator(index)),
+              )
+            ],
+          )
         ],
       ),
+    );
+  }
+
+  setCurrentPage(int value) {
+    currentPage = value;
+    setState(() {});
+  }
+
+  AnimatedContainer getIndicator(int pageNo) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 100),
+      height: 10,
+      width: (currentPage == pageNo) ? 20 : 10,
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          color: (currentPage == pageNo) ? Colors.orange : Colors.grey),
+    );
+  }
+
+  Widget onBoardPage(String img, String title) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 200,
+          padding: EdgeInsets.all(50),
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage('assets/images/$img.png')),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontSize: 30,
+              fontFamily: 'roboto',
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 40,
+          ),
+          child: Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
     );
   }
 }
